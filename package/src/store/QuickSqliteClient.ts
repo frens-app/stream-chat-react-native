a4cd2f05aa7c66452f366eb1d8fc2555af8813c7
Brian Li <brian.li.101@gmail.com>
2022-11-02 11:57:13 -0700

    implementing expo-sqlite

--- package/src/store/QuickSqliteClient.ts
+++ package/src/store/QuickSqliteClient.ts
@@ -1,9 +1,10 @@
 /* eslint-disable no-underscore-dangle */
-import type { QuickSQLite } from 'react-native-quick-sqlite';
-let sqlite: typeof QuickSQLite;
+// import type { QuickSQLite } from 'react-native-quick-sqlite';
+import * as SQLite from 'expo-sqlite';
+// let sqlite: typeof QuickSQLite;
 
 try {
-  sqlite = require('react-native-quick-sqlite')?.QuickSQLite;
+  // sqlite = require('react-native-quick-sqlite')?.QuickSQLite;
 } catch (e) {
   // Failed for one of the reason
   // 1. Running on expo, where we don't support offline storage yet.
@@ -20,6 +21,8 @@ import type { PreparedQueries, Table } from './types';
  * This way usage react-native-quick-sqlite package is scoped to a single class/file.
  */
 export class QuickSqliteClient {
+  static db: SQLite.WebSQLDatabase;
+
   static dbVersion = 2;
 
   static dbName = DB_NAME;
@@ -30,46 +33,69 @@ export class QuickSqliteClient {
   static setDbVersion = (version: number) => (this.dbVersion = version);
 
   static openDB = () => {
-    const { message, status } = sqlite.open(this.dbName, this.dbLocation);
-    sqlite.executeSql(this.dbName, `PRAGMA foreign_keys = ON`, []);
+    // const { message, status } = sqlite.open(this.dbName, this.dbLocation);
+    this.db = SQLite.openDatabase(this.dbName);
+    
+    // sqlite.executeSql(this.dbName, `PRAGMA foreign_keys = ON`, []);
+    this.db.transaction((tx) => {
+      tx.executeSql(`PRAGMA foreign_keys = ON`)
+    })
 
-    if (status === DB_STATUS_ERROR) {
-      console.error(`Error opening database ${this.dbName}: ${message}`);
-    }
+    // if (status === DB_STATUS_ERROR) {
+    //   console.error(`Error opening database ${this.dbName}: ${message}`);
+    // }
   };
 
   static closeDB = () => {
-    const { message, status } = sqlite.open(this.dbName, this.dbLocation);
+    // const { message, status } = sqlite.open(this.dbName, this.dbLocation);
+    this.db.closeAsync();
 
-    if (status === DB_STATUS_ERROR) {
-      console.error(`Error opening database ${this.dbName}: ${message}`);
-    }
+    // if (status === DB_STATUS_ERROR) {
+    //   console.error(`Error opening database ${this.dbName}: ${message}`);
+    // }
   };
 
   static executeSqlBatch = (queries: PreparedQueries[]) => {
     this.openDB();
 
-    const res = sqlite.executeSqlBatch(DB_NAME, queries);
+    // const res = sqlite.executeSqlBatch(DB_NAME, queries);
+    this.db.exec(queries, false, () => {});
 
-    if (res.status === 1) {
-      console.error(`Query/queries failed: ${res.message} ${JSON.stringify(res)}`);
-    }
+    // if (res.status === 1) {
+    //   console.error(`Query/queries failed: ${res.message} ${JSON.stringify(res)}`);
+    // }
 
     this.closeDB();
   };
 
   static executeSql = (query: string, params?: string[]) => {
+    console.log('executeSql: start')
     this.openDB();
 
-    const { message, rows, status } = sqlite.executeSql(DB_NAME, query, params);
+    // const { message, rows, status } = sqlite.executeSql(DB_NAME, query, params);
+    this.db.transaction((tx) => {
+      tx.executeSql(query, params, (transaction, resultSet) => {
+        console.log('executeSql: executed with rows ', resultSet.rows._array)
+        this.closeDB();
 
-    this.closeDB();
+        if(resultSet.rows) {
+          return resultSet.rows._array
+        } else {
+          return []
+        }
+      })
+    }, () => {
+      this.closeDB();
+      return [];
+    })
+    
+    // this.closeDB();
 
-    if (status === 1) {
-      console.error(`Query/queries failed: ${message}: `, query);
-    }
+    // if (status === 1) {
+    //   console.error(`Query/queries failed: ${message}: `, query);
+    // }
 
-    return rows ? rows._array : [];
+    // return rows ? rows._array : [];
   };
 
   static dropTables = () => {
@@ -82,10 +108,12 @@ export class QuickSqliteClient {
   };
 
   static deleteDatabase = () => {
-    const { message, status } = sqlite.delete(this.dbName, this.dbLocation);
-    if (status === DB_STATUS_ERROR) {
-      throw new Error(`Error deleting DB: ${message}`);
-    }
+    // const { message, status } = sqlite.delete(this.dbName, this.dbLocation);
+    this.db.deleteAsync();
+
+    // if (status === DB_STATUS_ERROR) {
+    //   throw new Error(`Error deleting DB: ${message}`);
+    // }
 
     return true;
   };
@@ -118,22 +146,27 @@ export class QuickSqliteClient {
   static updateUserPragmaVersion = (version: number) => {
     this.openDB();
 
-    sqlite.executeSql(DB_NAME, `PRAGMA user_version = ${version}`, []);
+    // sqlite.executeSql(DB_NAME, `PRAGMA user_version = ${version}`, []);
+    this.executeSql(`PRAGMA user_version = ${version}`)
 
     this.closeDB();
   };
 
   static getUserPragmaVersion = () => {
+    console.log('getUserPragmaVersion: openDB')
     this.openDB();
 
-    const { message, rows, status } = sqlite.executeSql(DB_NAME, `PRAGMA user_version`, []);
+    console.log('getUserPragmaVersion: executeSql')
+    // const { message, rows, status } = sqlite.executeSql(DB_NAME, `PRAGMA user_version`, []);
+    const rows = this.executeSql(`PRAGMA user_version`)
+    console.log('getUserPragmaVersion: rows ', rows)
 
     this.closeDB();
-    if (status === 1) {
-      console.error(`Querying for user_version failed: ${message}`);
-    }
-
-    const result = rows ? rows._array : [];
+    // if (status === 1) {
+    //   console.error(`Querying for user_version failed: ${message}`);
+    // }
+    console.log('getUserPragmaVersion: wrapping up')
+    const result = rows ? rows : [];
     return result[0].user_version as number;
   };
 
